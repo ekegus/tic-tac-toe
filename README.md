095ff2e7280b67a98cf66e5d95e59b6ef127d41d
Ben Busch <ben.busch@breedr.co>
2023-10-04 23:50:04 +0100

    WIP: Create a board struct

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tic Tac Toe Game
+- https://open.appacademy.io/learn/full-stack-online/software-engineering-foundations/tictactoe-project
+    Version 1 - Basic 3x3 grid game with 2 human players
+    Version 2 - Dynamic sized grid with 2 or more human players
+    Version 3 - Dynamic sized grid with 2 or more human or computer players
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+use anyhow::anyhow;
+
+struct Board {
+    grid: Vec<Vec<char>>,
+}
+
+impl Board {
+    fn new() -> Self {
+        Board {
+            grid: vec![
+                vec!['_', '_', '_'],
+                vec!['_', '_', '_'],
+                vec!['_', '_', '_'],
+            ],
+        }
+    }
+
+    fn print_board(&self) -> () {
+        self.grid.iter().for_each(|row| println!("{:?}", row));
+    }
+
+    fn is_position_empty(&self, position: [usize; 2]) -> bool {
+        match self.get_mark(position) {
+            Ok('_') => true,
+            _ => false,
+        }
+    }
+
+    fn get_mark(&self, position: [usize; 2]) -> anyhow::Result<char> {
+        let [row_position, column_position] = position;
+
+        let row = self
+            .grid
+            .get(row_position)
+            .ok_or(anyhow!("Selected position is out of bounds"))?;
+        let mark = row
+            .get(column_position)
+            .ok_or(anyhow!("Selected position is out of bounds"))?;
+
+        Ok(*mark)
+    }
+
+    fn is_position_valid(&self, position: [usize; 2]) -> bool {
+        match self.get_mark(position) {
+            Ok(_) => true,
+            _ => false,
+        }
+    }
+
+    fn place_mark(&mut self, mark: char, position: [usize; 2]) -> anyhow::Result<()> {
+        let [row_position, column_position] = position;
+
+        if self.is_position_valid(position) == false {
+            return Err(anyhow!("The position is outside the board"));
+        }
+
+        if self.is_position_empty(position) == false {
+            return Err(anyhow!("Position is already taken."));
+        }
+
+        self.grid[row_position][column_position] = mark;
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = Board::new();
+    board.print_board();
+
+    match board.place_mark('0', [0, 6]) {
+        Ok(value) => println!("Result: {:?}", value),
+        Err(error) => eprintln!("Error: {:?}", error),
+    }
+
+    board.print_board();
+
+    // let mark = board.get_mark([0, 1]);
+    // let is_empty = board.is_position_empty([0, 1]);
 }
