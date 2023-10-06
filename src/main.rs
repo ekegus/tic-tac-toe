c4f551444c3b5d6425d3c4645db188abacac7ef6
Ben Busch <ben.busch@breedr.co>
2023-10-06 21:39:56 +0100

    Rough iteration on board methods

--- src/main.rs
+++ src/main.rs
@@ -88,27 +88,64 @@ impl Board {
 
         has_won
     }
+
+    fn win_diagonal(&self, mark: char) -> bool {
+        let mut first_diagonal = true;
+        let mut second_diagonal = true;
+        let mut reversed_grid = self.grid.clone();
+        reversed_grid.reverse();
+
+        for i in 0..=2 {
+            if self.grid[i][i] != mark {
+                first_diagonal = false
+            };
+        }
+
+        for i in 0..=2 {
+            if reversed_grid[i][i] != mark {
+                second_diagonal = false
+            };
+        }
+
+        first_diagonal == true || second_diagonal == true
+    }
+
+    fn win(&self, mark: char) -> bool {
+        match (
+            self.win_row(mark),
+            self.win_col(mark),
+            self.win_diagonal(mark),
+        ) {
+            (true, _, _) | (_, true, _) | (_, _, true) => true,
+            _ => false,
+        }
+    }
+
+    fn empty_positions(&self) -> bool {
+        println!("{:?}", self.grid.concat());
+        self.grid.concat().iter().any(|position| position == &'_')
+    }
 }
 
 fn main() {
     let mut board = Board::new();
     board.print_board();
 
-    match board.place_mark('0', [0, 0]) {
-        Ok(value) => println!("Result: {:?}", value),
-        Err(error) => eprintln!("Error: {:?}", error),
-    }
-    match board.place_mark('0', [1, 0]) {
-        Ok(value) => println!("Result: {:?}", value),
-        Err(error) => eprintln!("Error: {:?}", error),
-    }
-    match board.place_mark('0', [2, 0]) {
-        Ok(value) => println!("Result: {:?}", value),
-        Err(error) => eprintln!("Error: {:?}", error),
-    }
+    // match board.place_mark('0', [0, 2]) {
+    //     Ok(value) => println!("Result: {:?}", value),
+    //     Err(error) => eprintln!("Error: {:?}", error),
+    // }
+    // match board.place_mark('0', [1, 1]) {
+    //     Ok(value) => println!("Result: {:?}", value),
+    //     Err(error) => eprintln!("Error: {:?}", error),
+    // }
+    // match board.place_mark('0', [2, 0]) {
+    //     Ok(value) => println!("Result: {:?}", value),
+    //     Err(error) => eprintln!("Error: {:?}", error),
+    // }
 
     board.print_board();
-    println!("{:?}", board.win_col('0'));
+    println!("{:?}", board.empty_positions());
 
     // let mark = board.get_mark([0, 1]);
     // let is_empty = board.is_position_empty([0, 1]);
