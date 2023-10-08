61a20b354899ef989ed8d5f4e5bef094852da0aa
Ben Busch <ben.busch@breedr.co>
2023-10-08 21:53:24 +0100

    Fix draw outcome and clean up code

--- src/tic_tac_toe/game.rs
+++ src/tic_tac_toe/game.rs
@@ -6,86 +6,73 @@ pub enum CurrentPlayer {
     Player2,
 }
 
-// TODO: Simplify the play function
-// TODO: Maybe introduce a game state...
+#[derive(PartialEq, Debug)]
+pub enum GameState {
+    InProgress,
+    GameOver,
+}
 
 pub struct Game {
     current_player: CurrentPlayer,
+    game_state: GameState,
 }
 
 impl Game {
     pub fn new() -> Self {
         Game {
             current_player: CurrentPlayer::Player1,
+            game_state: GameState::InProgress,
         }
     }
 
     pub fn switch_turn(&mut self) {
         if self.current_player == CurrentPlayer::Player1 {
             self.current_player = CurrentPlayer::Player2;
-            println!("{:?}", self.current_player);
         } else {
             self.current_player = CurrentPlayer::Player1;
-            println!("{:?}", self.current_player);
+        }
+    }
+
+    pub fn play_round(&mut self, board: &mut Board, player: &HumanPlayer) -> anyhow::Result<()> {
+        let position = player.get_position()?;
+        let mark = player.get_mark();
+
+        board.place_mark(mark, position)?;
+
+        if board.has_won(mark) {
+            let mark = player.get_mark();
+            println!("Player {} wins!", mark);
+            board.print_board();
+            self.game_state = GameState::GameOver;
+            Ok(())
+        } else {
+            self.switch_turn();
+            Ok(())
         }
     }
 
     pub fn play(&mut self, mut board: Board, player_1: HumanPlayer, player_2: HumanPlayer) -> () {
-        while board.get_has_empty_positions() == true {
+        while self.game_state == GameState::InProgress {
             board.print_board();
 
             if self.current_player == CurrentPlayer::Player1 {
-                match player_1.get_position() {
-                    Ok(position) => {
-                        let mark = player_1.get_mark();
-                        match board.place_mark(mark, position) {
-                            Ok(()) => {
-                                if board.has_won(mark) {
-                                    println!("Victory");
-                                    break;
-                                } else {
-                                    self.switch_turn();
-                                }
-                            }
-                            Err(err_message) => {
-                                println!("{}", err_message);
-                                continue;
-                            }
-                        };
-                    }
-                    Err(err_message) => {
-                        println!("{}", err_message);
-                        continue;
-                    }
-                }
+                if let Err(err) = self.play_round(&mut board, &player_1) {
+                    println!("{}", err);
+                    continue;
+                };
             } else {
-                match player_2.get_position() {
-                    Ok(position) => {
-                        let mark = player_2.get_mark();
-                        match board.place_mark(mark, position) {
-                            Ok(()) => {
-                                if board.has_won(mark) {
-                                    println!("Victory");
-                                    break;
-                                } else {
-                                    self.switch_turn();
-                                }
-                            }
-                            Err(err_message) => {
-                                println!("{}", err_message);
-                                continue;
-                            }
-                        };
-                    }
-                    Err(err_message) => {
-                        println!("{}", err_message);
-                        continue;
-                    }
-                }
+                if let Err(err) = self.play_round(&mut board, &player_2) {
+                    println!("{}", err);
+                    continue;
+                };
             }
-        }
 
-        // TODO: Fix this displaying no matter what
-        println!("Draw!");
+            if board.get_has_empty_positions() == false {
+                println!("Draw!");
+
+                board.print_board();
+                self.game_state = GameState::GameOver
+            };
+        }
     }
 }
